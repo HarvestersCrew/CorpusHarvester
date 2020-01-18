2f2b07578bac0eb071440e405560464ad9967143
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-01-18 16:50:34 +0100

    download_manager: better use of iterator.

--- src/download/download_manager.cpp
+++ src/download/download_manager.cpp
@@ -17,27 +17,26 @@ bool DownloadManager::add_url(const std::string &url) {
 
 bool DownloadManager::download_queue_to(const std::string &path) {
   FILE *f;
-  long unsigned int i = 0;
-  while (i < this->urls.size()) {
+  for (std::vector<std::string>::iterator it = this->urls.begin();
+       it != this->urls.end();) {
 
-    auto url = this->urls.at(i);
     std::stringstream path_with_filename;
     path_with_filename << path << "/" << random_str(this->FILENAME_LENGTH);
     f = fopen(path_with_filename.str().c_str(), "w");
     if (!f) {
       std::cout << "Can't write to " << path_with_filename.str()
-                << " file, skipping " << url << "..." << std::endl;
-      ++i;
+                << " file, skipping " << *it << "..." << std::endl;
+      ++it;
       continue;
     }
 
-    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_URL, (*it).c_str());
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, f);
     curl_easy_perform(curl);
     fclose(f);
-    std::cout << "Downloaded " << url << " to " << path_with_filename.str()
+    std::cout << "Downloaded " << *it << " to " << path_with_filename.str()
               << std::endl;
-    this->urls.erase(this->urls.begin() + i);
+    this->urls.erase(it);
   }
   return true;
 }
