53d1fcfd60ef7e02e63009d157b7a9a223df6d1a
JulienCHATEAU <jujugoal44@hotmail.fr>
2020-02-08 14:43:21 +0100

    woops

--- engine/src/ui/cli/CommandLineInterface.cpp
+++ engine/src/ui/cli/CommandLineInterface.cpp
@@ -41,13 +41,11 @@ Corpus CommandLineInterface::createCorpus() {
   // Download corresponding data
   download_manager dl;
   api_loader twitter(std::string("data/twitter.json"));
-  std::list<File *> out = twitter.query_and_parse(
-      {{"q", type},
-       {"Authorization",
-        "Bearer "
-        "AAAAAAAAAAAAAAAAAAAAAFbXBwEAAAAAffCqNtEEVChsyKEOcep3UtaqDgM%"
-        "3DDQyQagZBPQgrd02E4cnboMTDJlbfkObhWscuvXq3iXplX3TEXu"}},
-      dl);
+  std::list<File *> out =
+      twitter.query_and_parse({{"q", type},
+                               {"Authorization", "Bearer "
+                                                 "bearer_code"}},
+                              dl);
 
   // Index the downloaded data
   Indexer indexer("harvester", true);
