2d1fa3e11c0c6276ebdd326ec1802d60f7c31ad1
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-03-02 09:55:53 +0100

    Storage cleanup

--- engine/include/storage/storage.h
+++ engine/include/storage/storage.h
@@ -3,10 +3,12 @@
 
 #include "indexation/file.h"
 #include "utils/exceptions.h"
+#include <bits/stdc++.h>
 #include <fstream>
+#include <iostream>
 #include <string>
-
-using std::string;
+#include <sys/stat.h>
+#include <sys/types.h>
 
 /**
  * Storage class provides methods to store the files in the file system
@@ -17,21 +19,21 @@ private:
   /**
    * The name of the root folder where the files are stored
    */
-  string _root_folder_name;
+  std::string _root_folder_name;
 
   /**
    * Creates the missing folder in the given path from the root folder
    * @param folder_path the path to create from the root folder
    * @return 0 if something went wrong, 1 otherwise
    */
-  int create_folders_in_root(string folder_path) const;
+  int create_folders_in_root(std::string folder_path) const;
 
   /**
    * Checks if the given folder path exists fro mthe root folder
    * @param folder_path the path to create from the root folder
    * @return true if exists, false otherwise
    */
-  bool folder_exists_in_root(string folder_path) const;
+  bool folder_exists_in_root(std::string folder_path) const;
 
 public:
   /**
@@ -39,7 +41,7 @@ public:
    * @param root_folder_name the name of the root folder where the files are
    * stored
    */
-  Storage(const string root_folder_name);
+  Storage(const std::string root_folder_name);
 
   /**
    * Given the name of a file and its source (api) name, finds destination of
@@ -48,7 +50,8 @@ public:
    * @param api_name the source where this file was downloaded
    * @return the destination path of the file in the storage
    */
-  string file_destination(string file_name, string api_name) const;
+  std::string file_destination(std::string file_name,
+                               std::string api_name) const;
 
   /**
    * Moves the file from its current path to its destination in the storage
@@ -66,10 +69,10 @@ public:
    * Creates the files in the storage
    * @param files the files to store
    */
-  void store_files(list<File *> files) const;
+  void store_files(std::list<File *> files) const;
 
-  string get_root_folder_name() const { return _root_folder_name; }
-  void set_root_folder_name(string root_folder_name) {
+  std::string get_root_folder_name() const { return _root_folder_name; }
+  void set_root_folder_name(std::string root_folder_name) {
     _root_folder_name = root_folder_name;
   }
 };
--- engine/src/storage/storage.cpp
+++ engine/src/storage/storage.cpp
@@ -1,21 +1,13 @@
-#include <bits/stdc++.h>
-#include <iostream>
-#include <sys/stat.h>
-#include <sys/types.h>
-
 #include "storage/storage.h"
 
-using std::cout;
-using std::endl;
-
-Storage::Storage(const string root_folder_name)
+Storage::Storage(const std::string root_folder_name)
     : _root_folder_name((root_folder_name[root_folder_name.length() - 1] == '/')
                             ? root_folder_name
                             : root_folder_name + "/") {}
 
-int Storage::create_folders_in_root(string folder_path) const {
-  string dest_folder_name = _root_folder_name + folder_path;
-  string mkpath_cmd = "mkdir -p " + dest_folder_name;
+int Storage::create_folders_in_root(std::string folder_path) const {
+  std::string dest_folder_name = _root_folder_name + folder_path;
+  std::string mkpath_cmd = "mkdir -p " + dest_folder_name;
   if (system(mkpath_cmd.c_str()) == -1) {
     return 0;
   } else {
@@ -23,20 +15,21 @@ int Storage::create_folders_in_root(string folder_path) const {
   }
 }
 
-bool Storage::folder_exists_in_root(string folder_path) const {
+bool Storage::folder_exists_in_root(std::string folder_path) const {
   struct stat buffer;
-  string dest = _root_folder_name + folder_path;
+  std::string dest = _root_folder_name + folder_path;
   return (stat(dest.c_str(), &buffer) == 0);
 }
 
-string Storage::file_destination(string file_name, string api_name) const {
-  string dest_folder_name = api_name + "/" + file_name[0] + "/";
-  string dest_folder_path = _root_folder_name + dest_folder_name;
+std::string Storage::file_destination(std::string file_name,
+                                      std::string api_name) const {
+  std::string dest_folder_name = api_name + "/" + file_name[0] + "/";
+  std::string dest_folder_path = _root_folder_name + dest_folder_name;
   bool dest_folder_exists = folder_exists_in_root(dest_folder_name);
   if (!dest_folder_exists) {
     int correctly_created = create_folders_in_root(dest_folder_name);
     if (!correctly_created) {
-      string error_message = "Error creating : " + dest_folder_path;
+      std::string error_message = "Error creating : " + dest_folder_path;
       throw CommandException(error_message);
     }
   }
@@ -44,10 +37,11 @@ string Storage::file_destination(string file_name, string api_name) const {
 }
 
 void Storage::move_file(File *file) const {
-  string file_dest = file_destination(file->get_name(), file->get_source());
-  string move_file_cmd = "mv " + file->get_path() + " " + file_dest;
+  std::string file_dest =
+      file_destination(file->get_name(), file->get_source());
+  std::string move_file_cmd = "mv " + file->get_path() + " " + file_dest;
   if (system(move_file_cmd.c_str()) == -1) {
-    string error_message =
+    std::string error_message =
         "Error moving : " + file->get_path() + " to " + file_dest;
     throw CommandException(error_message);
   }
@@ -55,12 +49,13 @@ void Storage::move_file(File *file) const {
 }
 
 void Storage::store_file(File *file) const {
-  string file_dest = file_destination(file->get_name(), file->get_source());
+  std::string file_dest =
+      file_destination(file->get_name(), file->get_source());
   file->store(file_dest);
   file->set_path(file_dest);
 }
 
-void Storage::store_files(list<File *> files) const {
+void Storage::store_files(std::list<File *> files) const {
   for (auto &file : files) {
     store_file(file);
   }
