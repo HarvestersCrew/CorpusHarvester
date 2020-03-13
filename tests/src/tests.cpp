66b39ceddd9a7f75103d91a6109f3aaaafd3dc5f
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-03-13 10:46:07 +0100

    Drop tables after tests

--- tests/src/tests.cpp
+++ tests/src/tests.cpp
@@ -1,3 +1,4 @@
+#include "database/harvester_database.h"
 #include "test/cli_parser_test.h"
 #include "test/database_test.h"
 #include "test/indexation_test.h"
@@ -12,6 +13,8 @@ int main() {
   database_test();
   storage_test();
   indexation_test();
+  HarvesterDatabase::init();
+  HarvesterDatabase::drop();
   std::cout << std::endl
             << "Runs : "
             << Assertion::get_successes() + Assertion::get_failures()
