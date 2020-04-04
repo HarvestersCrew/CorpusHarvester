85500f193f1cab07a6aec7c8a0ea6bd062ce35e5
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-04-04 13:37:48 +0200

    Changing logger level default value when testing

--- tests/src/tests.cpp
+++ tests/src/tests.cpp
@@ -1,13 +1,18 @@
 #include "database/harvester_database.h"
+#include "database/setting.h"
 #include "test/apis_test.h"
 #include "test/cli_parser_test.h"
 #include "test/database_test.h"
 #include "test/indexation_test.h"
 #include "test/logger_test.h"
 #include "test/storage_test.h"
+#include "utils/logger.h"
 
 int main() {
   std::cout << std::endl << "Tests : " << std::endl;
+
+  Setting::set_default_value(Setting::LOGGER_LEVEL,
+                             std::to_string(logger::level::NONE));
 
   try {
     HarvesterDatabase::drop();
@@ -15,9 +20,7 @@ int main() {
   }
 
   // logger_test();
-  logger::set_level(logger::level::NONE);
   database_test();
-  logger::set_level(logger::level::NONE);
   storage_test();
   apis_test();
   cli_parser_test();
