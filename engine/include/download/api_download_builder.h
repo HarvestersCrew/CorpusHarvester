b11f8652eaeb6553b64cbea5bd90b48a92b3fc90
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-04-08 17:05:41 +0200

    API download builder can batch download

--- engine/include/download/api_download_builder.h
+++ engine/include/download/api_download_builder.h
@@ -1,6 +1,10 @@
 #ifndef API_DOWNLAD_BUILDER_H
 #define API_DOWNLAD_BUILDER_H
 
+#include "download/api_factory.h"
+#include "download/api_loader.h"
+#include "download/download_manager.h"
+#include "indexation/file.h"
 #include "utils/api_request_builder.h"
 #include <list>
 #include <memory>
@@ -12,8 +16,17 @@ using std::vector;
 
 class ApiDownloadBuilder : public ApiRequestBuilder {
 
+private:
+  download_manager dl;
+
 public:
   ApiDownloadBuilder();
+
+  /**
+   * Retrieves the file
+   * @throw api_factory_name_not_found checks before downloading anything if all
+   * APIs are found
+   */
   list<shared_ptr<File>> build();
 };
 
--- engine/src/download/api_download_builder.cpp
+++ engine/src/download/api_download_builder.cpp
@@ -4,5 +4,16 @@ ApiDownloadBuilder::ApiDownloadBuilder() : ApiRequestBuilder() {}
 
 list<shared_ptr<File>> ApiDownloadBuilder::build() {
   list<shared_ptr<File>> res;
+
+  // Throws an exception if a request API name is not found
+  for (const auto &el : this->get_requests()) {
+    ApiFactory::get_api(el.first);
+  }
+
+  for (const auto &el : this->get_requests()) {
+    shared_ptr<api_loader> api = ApiFactory::get_api(el.first);
+    res.splice(res.end(), api->query_and_parse(el.second, this->dl));
+  }
+
   return res;
 }
