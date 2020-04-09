c948da4aa8f579e9c41574a13f0cea053f7c046e
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-04-09 15:29:06 +0200

    Some comments

--- engine/src/download/api_download_builder.cpp
+++ engine/src/download/api_download_builder.cpp
@@ -18,19 +18,25 @@ list<shared_ptr<File>> ApiDownloadBuilder::build(int number) const {
       this->get_requests());
   long unsigned int downloaded_this_turn;
 
+  // For each specified request
   for (auto it = requests.begin(); it != requests.end();) {
 
+    // Get the api_loader
     shared_ptr<api_loader> api = ApiFactory::get_api(it->first);
 
+    // Do the query
     list<shared_ptr<File>> downloaded =
         api->query_and_parse(it->second, this->dl);
 
+    // If the returned download is empty, it means there is an error or there is
+    // nothing more to find, so we remove it from our local requests
     if (downloaded.size() > 0) {
       ++it;
     } else {
       it = requests.erase(it);
     }
 
+    // Only choose the files not present in the DB
     list<shared_ptr<File>> partial_res;
     for (const auto &el : downloaded) {
       if (!el->api_id_exists(HarvesterDatabase::init())) {
@@ -44,6 +50,7 @@ list<shared_ptr<File>> ApiDownloadBuilder::build(int number) const {
     // Index the downloaded data
     indexer.indexation(partial_res);
 
+    // Add freshly downloaded data to the return list
     res.splice(res.end(), partial_res);
   }
 
