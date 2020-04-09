a24678ab88e8c0a8d6727d8a0fce5697ad6b8c46
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-04-09 15:26:10 +0200

    Moved the check of the exitances of downloads to the builder

--- engine/src/download/api_download_builder.cpp
+++ engine/src/download/api_download_builder.cpp
@@ -3,26 +3,45 @@
 ApiDownloadBuilder::ApiDownloadBuilder() : ApiRequestBuilder() {}
 
 list<shared_ptr<File>> ApiDownloadBuilder::build(int number) const {
+
   list<shared_ptr<File>> res;
+  sql::Connection *db = HarvesterDatabase::init();
+  Indexer indexer(db);
+  Storage storage(db);
 
   // Throws an exception if a request API name is not found
   for (const auto &el : this->get_requests()) {
     ApiFactory::get_api(el.first);
   }
 
-  for (const auto &el : this->get_requests()) {
-    shared_ptr<api_loader> api = ApiFactory::get_api(el.first);
+  vector<pair<string, unordered_map<string, string>>> requests(
+      this->get_requests());
+  long unsigned int downloaded_this_turn;
 
-    list<shared_ptr<File>> partial_res =
-        api->query_and_parse(el.second, this->dl);
+  for (auto it = requests.begin(); it != requests.end();) {
+
+    shared_ptr<api_loader> api = ApiFactory::get_api(it->first);
+
+    list<shared_ptr<File>> downloaded =
+        api->query_and_parse(it->second, this->dl);
+
+    if (downloaded.size() > 0) {
+      ++it;
+    } else {
+      it = requests.erase(it);
+    }
+
+    list<shared_ptr<File>> partial_res;
+    for (const auto &el : downloaded) {
+      if (!el->api_id_exists(HarvesterDatabase::init())) {
+        partial_res.push_back(el);
+      }
+    }
 
     // Store the files
-    sql::Connection *db = HarvesterDatabase::init();
-    Storage storage(db);
     storage.store_files(partial_res);
 
     // Index the downloaded data
-    Indexer indexer(db);
     indexer.indexation(partial_res);
 
     res.splice(res.end(), partial_res);
