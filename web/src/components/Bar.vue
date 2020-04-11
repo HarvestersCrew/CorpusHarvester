c5d1ce19ca1672ca8af3318e71257e6f5c21a540
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-04-11 12:24:29 +0200

    Vue lint

--- web/src/components/Bar.vue
+++ web/src/components/Bar.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <v-navigation-drawer app v-model="drawer" color="blue" mini-variant permanent>
+    <v-navigation-drawer
+      app
+      v-model="drawer"
+      color="blue"
+      mini-variant
+      permanent
+    >
       <v-list dense nav>
         <v-list-item
           v-for="item in items"
@@ -21,14 +27,14 @@
       <template v-slot:append>
         <v-list dense nav>
           <v-list-item
-          v-for="item in items2"
-          :key="item.title"
-          :to="{ name: item.title }"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon color="white">{{ item.icon }}</v-icon>
-          </v-list-item-icon>
+            v-for="item in items2"
+            :key="item.title"
+            :to="{ name: item.title }"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon color="white">{{ item.icon }}</v-icon>
+            </v-list-item-icon>
 
             <v-list-item-content>
               <v-list-item-title>{{ item.title }}</v-list-item-title>
