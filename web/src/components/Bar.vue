e784e3b860fc194076a7fbd0ae6f458252fbfb89
Hinageshi <mariebouin@protonmail.com>
2020-04-11 11:10:20 +0200

    Bar component

---
+++ web/src/components/Bar.vue
@@ -0,0 +1,68 @@
+<template>
+    <v-navigation-drawer
+        v-model="drawer"
+        color="blue"
+        mini-variant
+        permanent
+    >
+    <v-list
+          dense
+          nav
+        >
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon 
+                color="white"
+              >{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-divider 
+            class="my-2"
+            light
+          ></v-divider>
+
+          <v-list-item
+            v-for="item in items2"
+            :key="item.title"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon 
+                color="white"
+              >{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            drawer: true,
+            items: [
+                { title: 'Files', icon: 'mdi-pencil' },
+                { title: 'Corpus', icon: 'mdi-file' }
+            ],
+            items2: [
+                { title: 'Settings', icon: 'mdi-settings' },
+                { title: 'Info', icon: 'mdi-information-outline' }
+            ]
+        }
+    } 
+}
+</script>
