d43c98591205297f952ffc922b3e179dfb173a8e
Hinageshi <mariebouin@protonmail.com>
2020-04-11 11:49:52 +0200

    Changed Bar.vue

--- web/src/components/Bar.vue
+++ web/src/components/Bar.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <v-navigation-drawer
-      app
-      v-model="drawer"
-      color="blue"
-      mini-variant
-      permanent
-    >
+    <v-navigation-drawer app v-model="drawer" color="blue" mini-variant permanent>
       <v-list dense nav>
         <v-list-item v-for="item in items" :key="item.title" :to="{ name: item.title }" link>
           <v-list-item-icon>
@@ -17,19 +11,21 @@
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-
-        <v-divider class="my-2" light></v-divider>
-
-        <v-list-item v-for="item in items2" :key="item.title" :to="{ name: item.title }" link>
-          <v-list-item-icon>
-            <v-icon color="white">{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
       </v-list>
+
+      <template v-slot:append>
+        <v-list dense nav>
+          <v-list-item v-for="item in items2" :key="item.title" :to="{ name: item.title }" link>
+            <v-list-item-icon>
+              <v-icon color="white">{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </template>
     </v-navigation-drawer>
 
     <v-content>
