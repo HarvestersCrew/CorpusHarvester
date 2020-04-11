f3e927441a16f51d1b9eeed3baf5790f1831adc5
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-04-11 10:08:49 +0200

    Moved base files to .old files

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <v-app>
+      <router-view />
+    </v-app>
   </div>
 </template>
 
