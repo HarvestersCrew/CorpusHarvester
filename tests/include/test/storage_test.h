5c502072e643543b43c99309c6c5bb0352f5ae1e
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-04-13 19:00:06 +0200

    Removed unused script

--- tests/include/test/storage_test.h
+++ tests/include/test/storage_test.h
@@ -12,8 +12,6 @@
 #include <memory>
 #include <sys/stat.h>
 
-#define INIT_DATA "/project/engine/script/init_storage_data 100"
-
 void test_file_destination();
 void test_move_one_file();
 void test_store_one_file();
