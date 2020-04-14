9d7e7756dcda4f4a12a811ef2e6d43d04de32574
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-04-14 22:01:35 +0200

    Server added retries for connecting to the DB

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -1,11 +1,27 @@
 #include "database/harvester_database.h"
 #include "server/websocket_server.h"
+#include <chrono>
+#include <cppconn/exception.h>
+#include <iostream>
+#include <thread>
 
 using std::cout;
 using std::endl;
 
 int main(int argc, char **argv) {
-  HarvesterDatabase::init();
+  bool db_ready = false;
+
+  do {
+    try {
+      HarvesterDatabase::init();
+      db_ready = true;
+    } catch (const sql::SQLException &e) {
+      std::cout << "Failure connecting to DB, will retry in 10 seconds"
+                << std::endl;
+      std::this_thread::sleep_for(std::chrono::seconds(10));
+    }
+  } while (!db_ready);
+
   WebsocketServer::init();
   WebsocketServer::run();
 }
