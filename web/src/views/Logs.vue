1e22bdbcdc1ae8f8fae35e86f911da2b7f5dd878
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-04-15 10:57:41 +0200

    Removed buttons in logs

--- web/src/views/Logs.vue
+++ web/src/views/Logs.vue
@@ -3,10 +3,6 @@
     <v-system-bar window dark fixed id="system-bar">
       <v-icon>mdi-alert</v-icon>
       <span>Logs</span>
-      <v-spacer></v-spacer>
-      <v-icon>mdi-minus</v-icon>
-      <v-icon>mdi-checkbox-blank-outline</v-icon>
-      <v-icon>mdi-close</v-icon>
     </v-system-bar>
     <div id="logs">
       <v-list-item v-for="(log, index) in $store.state.logs" :key="index">
