25ad28ff7832667e00c6d4d5a6ce200476f27e90
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-04-16 22:35:02 +0200

    PoolDB in CLI launcher

--- cli/src/cli.cpp
+++ cli/src/cli.cpp
@@ -1,6 +1,9 @@
 
 #include "cli/CommandLineInterface.h"
+#include "database/pool_db.h"
+#include <chrono>
 #include <iostream>
+#include <thread>
 
 /**
  * Get the command of the user.
@@ -10,6 +13,18 @@
  * @return
  */
 int main(int argc, char **argv) {
+
+  bool db_ready = false;
+  do {
+    try {
+      PoolDB::init(10);
+      db_ready = true;
+    } catch (const sql::SQLException &e) {
+      std::cout << "Failure connecting to DB, will retry in 10 seconds"
+                << std::endl;
+      std::this_thread::sleep_for(std::chrono::seconds(10));
+    }
+  } while (!db_ready);
 
   CommandLineInterface cli = CommandLineInterface(argc, argv);
   cli.run();
