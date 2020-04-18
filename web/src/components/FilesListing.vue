be1300d71d3927574bceb06c5c9517bab2171f00
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-04-18 22:29:28 +0200

    Added content to empty files listing

--- web/src/components/FilesListing.vue
+++ web/src/components/FilesListing.vue
@@ -1,27 +1,42 @@
 <template>
-  <v-list>
-    <div v-for="(item, index) in $store.state.downloaded_files" :key="index">
-      <v-list-item>
-        <v-checkbox></v-checkbox>
-        <v-list-item-content>
+  <div>
+    <v-list v-if="$store.state.downloaded_files.length > 0">
+      <div v-for="(item, index) in $store.state.downloaded_files" :key="index">
+        <v-list-item>
           <v-row dense>
             <v-col>{{ item.name }}.{{ item.format }}</v-col>
             <v-col>{{ item.size }} bytes</v-col>
-            <v-col>{{ item.source }}</v-col>
+            <v-col>{{ item.source }} ({{ api_type(item.source) }})</v-col>
           </v-row>
-        </v-list-item-content>
-        <v-list-item-icon>
-          <v-btn icon><v-icon>mdi-dots-vertical</v-icon></v-btn>
-        </v-list-item-icon>
-      </v-list-item>
-      <v-divider
-        v-if="index + 1 < $store.state.downloaded_files.length"
-      ></v-divider>
-    </div>
-  </v-list>
+        </v-list-item>
+        <v-divider
+          v-if="index + 1 < $store.state.downloaded_files.length"
+        ></v-divider>
+      </div>
+    </v-list>
+
+    <v-container v-else>
+      <v-row justify="center">
+        <v-col cols="10" md="6">
+          <v-card>
+            <v-card-title>How to download</v-card-title>
+            <v-card-text class="text-left">
+              Start by adding requests and filling needed parameters. Once you
+              validate it, the download will start and you'll just have to wait.
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 <script>
 export default {
-  name: "FilesListing"
+  name: "FilesListing",
+  methods: {
+    api_type(api_name) {
+      return this.$store.getters.api_by_name(api_name).api_type;
+    }
+  }
 };
 </script>
