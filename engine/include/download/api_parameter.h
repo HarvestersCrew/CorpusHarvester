7fbe91a45517f9b6bb5994dd5c758ae646e33aa4
Thibault Nocchi <1619359+ThibaultNocchi@users.noreply.github.com>
2020-05-07 17:10:46 +0200

    Engine: added default descriptions to some API parameters

--- engine/include/download/api_parameter.h
+++ engine/include/download/api_parameter.h
@@ -8,6 +8,7 @@
 #include <optional>
 #include <sstream>
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #define API_PARAMETER_INT "int"
@@ -16,6 +17,7 @@
 
 using std::optional;
 using std::string;
+using std::unordered_map;
 using std::vector;
 
 /**
@@ -41,9 +43,9 @@ public:
 
   /**
    * String of current state
-   * @return std::string state of the parameter
+   * @return string state of the parameter
    */
-  virtual std::string to_string() const;
+  virtual string to_string() const;
 
   /**
    * JSON serialization of parameter
@@ -53,7 +55,7 @@ public:
   /**
    * Gets the string associated to the parameter type
    */
-  virtual std::string get_value_type_string() const;
+  virtual string get_value_type_string() const;
 
   /** Gets the parameter type */
   virtual api_parameter_base::value_type get_value_type() const;
@@ -61,9 +63,9 @@ public:
   /**
    * Converts the given JSON value to a string based on its type
    * @param val JSON value to convert
-   * @return std::string string associated to the value
+   * @return string string associated to the value
    */
-  virtual std::string json_value_to_string(const nlohmann::json &val) const;
+  virtual string json_value_to_string(const nlohmann::json &val) const;
 
   virtual string get_name() const;
 
@@ -82,15 +84,20 @@ protected:
   virtual ~api_parameter_base(){};
 
   /** Name of the parameter on the API side */
-  std::string _api_name;
+  string _api_name;
   /** Display name of the tag (DB, selector...) */
-  std::string _name;
+  string _name;
   /** Type of the parameter values */
   value_type _value_type;
   /** Whether the parameter is relevant to keep in the database */
   bool _relevant;
   /** Description of the parameter, optional */
   optional<string> _description;
+
+  /**
+   * Is filled with default descriptions to be used
+   */
+  static unordered_map<string, string> _default_descriptions;
 };
 
 /**
@@ -122,9 +129,9 @@ public:
 
   /**
    * String of current state
-   * @return std::string state of the parameter
+   * @return string state of the parameter
    */
-  virtual std::string to_string() const;
+  virtual string to_string() const;
 
   /**
    * JSON serialization of parameter
@@ -137,14 +144,14 @@ public:
    * @param val value to check
    * @return whether the value is valid for the parameter
    */
-  bool is_value_valid(const std::string &val) const;
+  bool is_value_valid(const string &val) const;
 
   /**
    * Checks if a given value as a string can be typed to the parameter type
    * @param val value to check
    * @return whether the value is correctly typed
    */
-  bool is_value_correctly_typed(const std::string &val) const;
+  bool is_value_correctly_typed(const string &val) const;
 
   /**
    * Adds the given value as a default value
@@ -152,7 +159,7 @@ public:
    * @throw api_parameter_incompatible_value if value is incompatible to the
    * object
    */
-  void set_default_value(const std::string &val);
+  void set_default_value(const string &val);
 
   bool get_required() const;
   optional<string> get_default_value() const;
@@ -160,13 +167,13 @@ public:
 
 private:
   /** Position of the parameter in the HTTP request (body, header...) */
-  std::string _position;
+  string _position;
   /** Whether the parameter is required in a request */
   bool _required;
   /** Default value to use in requests */
-  std::optional<std::string> _default_value;
+  std::optional<string> _default_value;
   /** Enum of values valid to use */
-  std::vector<std::string> _values;
+  std::vector<string> _values;
   /** Whether the parameter should be exposed to user, true by default */
   bool _is_public;
 };
@@ -184,11 +191,11 @@ class api_parameter_response : public api_parameter_base {
 private:
   /** Values to prepend to responses, string is the value and bool is if it
    * is a parameter's name or a direct string */
-  std::vector<std::pair<std::string, bool>> _string_prepends;
+  std::vector<std::pair<string, bool>> _string_prepends;
 
   /** Values to append to responses, string is the value and bool is if it
    * is a parameter's name or a direct string */
-  std::vector<std::pair<std::string, bool>> _string_appends;
+  std::vector<std::pair<string, bool>> _string_appends;
 
 public:
   /**
@@ -199,9 +206,9 @@ public:
 
   /**
    * String of current state
-   * @return std::string state of the parameter
+   * @return string state of the parameter
    */
-  virtual std::string to_string() const;
+  virtual string to_string() const;
 
   /**
    * JSON serialization of parameter
--- engine/src/download/api_parameter.cpp
+++ engine/src/download/api_parameter.cpp
@@ -1,10 +1,16 @@
 #include "download/api_parameter.h"
 
+unordered_map<string, string> api_parameter_base::_default_descriptions{
+    {"_api_id", "Unique ID of an element for this API"},
+    {"_page", "Page number to use or start at for queries"},
+    {"_page_count", "Number of elements per page"},
+    {"query", "Search query"}};
+
 api_parameter_base::api_parameter_base(const nlohmann::json &json) {
 
-  this->_api_name = json.at("api_name").get<std::string>();
+  this->_api_name = json.at("api_name").get<string>();
   if (json.contains("name")) {
-    this->_name = json.at("name").get<std::string>();
+    this->_name = json.at("name").get<string>();
   } else {
     this->_name = this->_api_name;
   }
@@ -14,20 +20,22 @@ api_parameter_base::api_parameter_base(const nlohmann::json &json) {
   this->_description = std::nullopt;
   if (json.contains("description")) {
     this->_description = json.at("description");
+  } else if (_default_descriptions.find(_name) != _default_descriptions.end()) {
+    _description = _default_descriptions.at(_name);
   }
 
-  if (json.at("type").get<std::string>() == API_PARAMETER_STRING) {
+  if (json.at("type").get<string>() == API_PARAMETER_STRING) {
     this->_value_type = value_type::STRING;
-  } else if (json.at("type").get<std::string>() == API_PARAMETER_INT) {
+  } else if (json.at("type").get<string>() == API_PARAMETER_INT) {
     this->_value_type = value_type::INT;
-  } else if (json.at("type").get<std::string>() == API_PARAMETER_IMAGE_LINK) {
+  } else if (json.at("type").get<string>() == API_PARAMETER_IMAGE_LINK) {
     this->_value_type = value_type::IMAGE_LINK;
   } else {
     throw std::runtime_error("Unrecognized type for parameter.");
   }
 }
 
-std::string api_parameter_base::to_string() const {
+string api_parameter_base::to_string() const {
   std::stringstream out;
   out << "------- api_name: " << this->_api_name << " -------" << std::endl;
   out << "name: " << this->_name << std::endl;
@@ -49,7 +57,7 @@ nlohmann::json api_parameter_base::serialize() const {
   return j;
 }
 
-std::string api_parameter_base::get_value_type_string() const {
+string api_parameter_base::get_value_type_string() const {
   switch (this->_value_type) {
   case api_parameter_base::value_type::INT:
     return API_PARAMETER_INT;
@@ -70,16 +78,16 @@ api_parameter_base::value_type api_parameter_base::get_value_type() const {
   return this->_value_type;
 }
 
-std::string
+string
 api_parameter_base::json_value_to_string(const nlohmann::json &val) const {
-  std::string result;
+  string result;
   try {
     if (this->_value_type == value_type::INT) {
       result = std::to_string(val.get<int>());
     } else if (this->_value_type == value_type::STRING) {
-      result = val.get<std::string>();
+      result = val.get<string>();
     } else if (this->_value_type == value_type::IMAGE_LINK) {
-      result = val.get<std::string>();
+      result = val.get<string>();
     }
   } catch (const nlohmann::json::exception &e) {
     logger::error("Error parsing a JSON value to a string");
@@ -109,7 +117,7 @@ api_parameter_base::get_sql_cast_prepared_string(const string &param) const {
 
 api_parameter_request::api_parameter_request(const nlohmann::json &json)
     : api_parameter_base(json) {
-  this->_position = json.at("position").get<std::string>();
+  this->_position = json.at("position").get<string>();
   this->_required = json.at("required").get<bool>();
 
   this->_is_public = true;
@@ -130,14 +138,14 @@ api_parameter_request::api_parameter_request(const nlohmann::json &json)
   }
 
   if (json.contains("default_value")) {
-    this->set_default_value(json.at("default_value").get<std::string>());
+    this->set_default_value(json.at("default_value").get<string>());
   } else {
     this->_default_value = std::nullopt;
   }
 }
 
-std::string api_parameter_request::to_string() const {
-  std::string base = api_parameter_base::to_string();
+string api_parameter_request::to_string() const {
+  string base = api_parameter_base::to_string();
   std::stringstream out;
   out << base << std::endl;
   out << "position: " << this->_position << std::endl;
@@ -147,7 +155,7 @@ std::string api_parameter_request::to_string() const {
       << std::endl;
 
   out << "values: ";
-  for (const std::string &el : this->_values)
+  for (const string &el : this->_values)
     out << el << ", ";
   return out.str();
 }
@@ -164,7 +172,7 @@ nlohmann::json api_parameter_request::serialize() const {
   return j;
 }
 
-bool api_parameter_request::is_value_valid(const std::string &val) const {
+bool api_parameter_request::is_value_valid(const string &val) const {
 
   if (!this->is_value_correctly_typed(val)) {
     return false;
@@ -179,8 +187,7 @@ bool api_parameter_request::is_value_valid(const std::string &val) const {
   return true;
 }
 
-bool api_parameter_request::is_value_correctly_typed(
-    const std::string &val) const {
+bool api_parameter_request::is_value_correctly_typed(const string &val) const {
 
   if (this->_value_type == value_type::INT) {
     try {
@@ -198,7 +205,7 @@ bool api_parameter_request::is_value_correctly_typed(
   return true;
 }
 
-void api_parameter_request::set_default_value(const std::string &val) {
+void api_parameter_request::set_default_value(const string &val) {
   if (!this->is_value_valid(val))
     throw api_parameter_incompatible_value(this->get_value_type_string(),
                                            this->get_name());
@@ -220,7 +227,7 @@ api_parameter_response::api_parameter_response(const nlohmann::json &json)
 
   if (json.contains("string_prepends")) {
     for (const auto &el : json.at("string_prepends")) {
-      std::string param_name = el.at("value").get<std::string>();
+      string param_name = el.at("value").get<string>();
       this->_string_prepends.push_back(
           std::make_pair(param_name, el.at("is_parameter_name").get<bool>()));
     }
@@ -228,26 +235,26 @@ api_parameter_response::api_parameter_response(const nlohmann::json &json)
 
   if (json.contains("string_appends")) {
     for (const auto &el : json.at("string_appends")) {
-      std::string param_name = el.at("value").get<std::string>();
+      string param_name = el.at("value").get<string>();
       this->_string_appends.push_back(
           std::make_pair(param_name, el.at("is_parameter_name").get<bool>()));
     }
   }
 }
 
-std::string api_parameter_response::to_string() const {
-  std::string base = api_parameter_base::to_string();
+string api_parameter_response::to_string() const {
+  string base = api_parameter_base::to_string();
   std::stringstream out;
   out << base << std::endl;
   out << "name: " << this->_name << std::endl;
   out << "string prepend: [" << std::endl;
-  for (std::pair<std::string, bool> el : this->_string_prepends) {
+  for (std::pair<string, bool> el : this->_string_prepends) {
     out << "value: \"" << el.first << "\", is_parameter: " << el.second
         << std::endl;
   }
   out << "]" << std::endl;
   out << "string append: [" << std::endl;
-  for (std::pair<std::string, bool> el : this->_string_appends) {
+  for (std::pair<string, bool> el : this->_string_appends) {
     out << "value: \"" << el.first << "\", is_parameter: " << el.second
         << std::endl;
   }
